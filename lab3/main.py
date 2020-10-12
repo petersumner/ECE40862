a6f50079d52dac1a669a50173345c7647637b35d
Peter <petersumner515@gmail.com>
2020-10-12 15:03:44 -0400

    lab 3 progress

--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,6 @@
-from machine import TouchPad, Pin
+from machine import TouchPad, Pin, Timer
 import network
+import ntptime
 
 led_red = Pin(14, Pin.OUT)
 led_green = Pin(15, Pin.OUT)
@@ -10,18 +11,24 @@ switch2 = Pin(36, Pin.IN)
 touch1 = TouchPad(Pin(12))
 touch2 = TouchPad(Pin(13))
 
-essid = "ThisLANisyourLAN"
-password = "spicy!avocad0"
+timer_date = Timer(0)
+timer_touch = Timer(1)
+timer_sleep = Timer(2)
+
+essid = "ThePromisedLAN"
+password = "fourwordsalluppercase"
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(essid, password)
 
 while not wlan.isconnected():
+    print("connecting...")
     pass
 
-print("Oh Yes! Get connected\nConnected to " + essid)
 macBytes = str(wlan.config('mac'))
+print("Oh Yes! Get connected\nConnected to " + essid)
 print("MAC Address: " + ':'.join('%02x' % ord(b) for b in macBytes))
 print("IP Address: " + str(wlan.ifconfig()[0]))
 
+ntptime.settime()
