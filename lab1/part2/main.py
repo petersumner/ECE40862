5334ac7be59b1f5be75b7ce7de68e7a48216e901
Peter <petersumner515@gmail.com>
2020-10-12 14:09:12 -0400

    add comments

--- lab1/part2/main.py
+++ lab1/part2/main.py
@@ -1,10 +1,12 @@
 from machine import Pin
 from time import sleep
 
+# Onboard RED LED connected to IO_13
 led_board = Pin(13, Pin.OUT)
 
+# Toggle LED 5 tiems
 for i in range(10):
     led_board.value(not led_board.value())
-    sleep(0.5)
+    sleep(0.5) # 0.5 seconds delay
 
 print("Led blinked 5 times")
--- lab1/part2/pattern.py
+++ lab1/part2/pattern.py
@@ -2,13 +2,26 @@ from machine import Pin
 from time import sleep
 import sys
     
+# Push button switches configured as GPIO inputs
 switch1 = Pin(14, Pin.IN)
 switch2 = Pin(15, Pin.IN)
+
+# External LEDs configured as GPIO outputs
 led_red = Pin(32, Pin.OUT)
 led_green = Pin(33, Pin.OUT)
 
 count1, count2 = 0, 0
 
+# LEDs will light when buttons are pressed as per the pattern below
+# ----------------------------------------
+# Switch1   Switch2   Red_LED   Green_LED
+#   OFF       OFF       OFF        OFF
+#   OFF       ON        OFF        ON
+#   ON        OFF       ON         OFF
+#   ON        ON        OFF        OFF
+# ----------------------------------------
+# Pattern stops and LEDs blink alternatively if one button has been 
+# pressed 10 times, program exits when the other button is pressed
 while True:
     
     switch1_state = switch1.value()
@@ -39,7 +52,8 @@ while True:
                 led_red.value(not led_red.value())
                 led_green.value(not led_green.value())
                 alternate = 0
-            if (count1 == 10 and switch2.value() == 1) or (count2 == 10 and switch1.value() == 1):
+            if (count1 == 10 and switch2.value() == 1) or 
+               (count2 == 10 and switch1.value() == 1):
                 led_red.value(0)
                 led_green.value(0)
                 print("You have sucessfully implemented LAB1 DEMO!!!")
